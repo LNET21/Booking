74b688631729978edfa5e071ea5390393d2c72da
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-09-20 13:53:33 +0200

    GymClass, ApplicationUser

---
+++ Booking.Core/Models/Entities/GymClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Core.Models.Entities
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime => StartDate + Duration;
+        public string Description { get; set; }
+    }
+}
