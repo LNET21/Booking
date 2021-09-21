61d865de7e4b0bdf838fd87a5d0c8df51ccf2b87
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-09-21 14:40:40 +0200

    BookingToogle

--- Booking.Data/Data/Migrations/20210921123648_UserGyms.Designer.cs
+++ Booking.Data/Data/Migrations/20210921123648_UserGyms.Designer.cs
@@ -4,14 +4,16 @@ using Booking.Data.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Booking.Data.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210921123648_UserGyms")]
+    partial class UserGyms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Booking.Web/Controllers/GymClassesController.cs
+++ Booking.Web/Controllers/GymClassesController.cs
@@ -7,22 +7,69 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Booking.Core.Models.Entities;
 using Booking.Data.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace Booking.Web.Controllers
 {
     public class GymClassesController : Controller
     {
         private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public GymClassesController(ApplicationDbContext context)
+        public GymClassesController(ApplicationDbContext context,UserManager<ApplicationUser> userManager)
         {
-            db = context;
+            db = context ?? throw new ArgumentNullException(nameof(context));
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         }
 
         // GET: GymClasses
         public async Task<IActionResult> Index()
         {
             return View(await db.GymClasses.ToListAsync());
+        }
+
+        [Authorize]
+        public async Task<IActionResult> BookingToggle(int? id)
+        {
+            if (id is null) return BadRequest();
+
+            var userId = userManager.GetUserId(User);
+            //Check for null
+
+            //var userId = (await userManager.GetUserAsync(User))?.Id;
+            //var userId2 = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+
+            //var currentGymClass = await db.GymClasses
+            //    .Include(g => g.AttendingMembers)
+            //    .FirstOrDefaultAsync(a => a.Id == id);
+
+            //var attending = currentGymClass?.AttendingMembers
+            //                                .FirstOrDefault(a => a.ApplicationUserId == userId);
+
+            var attending = await db.ApplicationUserGyms.FindAsync(userId, id);
+
+            if(attending is null)
+            {
+                var booking = new ApplicationUserGymClass
+                {
+                    ApplicationUserId = userId,
+                    GymClassId = (int)id
+                };
+
+                db.ApplicationUserGyms.Add(booking);
+            }
+            else
+            {
+                db.ApplicationUserGyms.Remove(attending);
+            }
+
+            await db.SaveChangesAsync();
+
+
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: GymClasses/Details/5
